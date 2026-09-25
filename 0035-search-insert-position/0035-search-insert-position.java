9d25f25aec0810fcf21f2bec993cbb6500def71e
Srishti Kapoor <srishtikapoor2201@gmail.com>
2026-09-25 18:06:54 +0530

    Time: 0 ms (100%), Space: 44.8 MB (29.26%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+        int low=0;
+        int high=nums.length-1;
+        int idx=nums.length;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(nums[mid]>=target){
+                idx=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+                
+            }
+        }
+        return idx;
+    }
+}
